9df6bb8775494b92fc70868776d5a2c1e290bbbe
IceCreamEngineer <mpistacchio77@gmail.com>
2020-11-25 17:52:34 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# spring-microservices
+ Repo for what I build in the "Master Microservices with Spring Boot and Spring Cloud" Udemy course.
---
+++ restful-web-services/src/test/java/com/training/microservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.microservices.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
