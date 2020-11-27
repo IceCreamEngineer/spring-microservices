7fb14e2edf230d46ba66832ba85f2133f8b76baa
IceCreamEngineer <mpistacchio77@gmail.com>
2020-11-27 12:40:16 -0500

    Stood up example controller to output desired strings to a webpage and also dynamically update with a provided URI/URL

---
+++ restful-web-services/src/main/java/com/training/microservices/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.training.microservices.restfulwebservices;
+
+/**
+ * @author Matt
+ *
+ */
+public class HelloWorldBean {
+	
+	private String message;
+
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+	
+}
---
+++ restful-web-services/src/main/java/com/training/microservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.training.microservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Matt
+ *
+ */
+//Controller
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping(path="/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	//hello-world-bean
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+	
+	//hello-world-bean/in28minutes
+	@GetMapping(path="/hello-world-bean/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	
+}
